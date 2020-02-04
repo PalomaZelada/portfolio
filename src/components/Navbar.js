46b15f54c713d805f1bdc055984f21faa74a6493
PalomaZelada <p.zelada.m@gmail.com>
2020-02-04 12:00:58 -0300

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,12 @@ import Navbar from 'react-bootstrap/Navbar'
 class Navbar1 extends Component{
     render(){
         return (
-            <Nav className="justify-content-end" defaultActiveKey="/home" as="ul">
+            <Nav className="justify-content-left" defaultActiveKey="/home" as="ul">
     <Navbar.Brand href="/home">
       <img
-        src={"//images/favicon.jpg"}
+        src="https://cdn1.vectorstock.com/i/1000x1000/13/95/initial-letter-pz-logo-template-design-vector-22671395.jpg"
+        width="40"
+        height="40"
         alt="PZ"
       />
     </Navbar.Brand>
