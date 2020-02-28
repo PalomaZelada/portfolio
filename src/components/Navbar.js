a1db8a9a247413678d0b549db54fd330a302202a
PalomaZelada <p.zelada.m@gmail.com>
2020-02-28 12:47:28 -0300

    Añadiendo responsive para pequeñas pantallas y arreglando imagenes de proyectos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ class Navbar1 extends Component {
     return (
       <div>
       <nav className="navbar-style">
-                    <a href="#home"><button className="btnNav">Sobre mi</button></a>
-                    <a href="#skills"><button className="btnNav">Herramientas</button></a>
-                    <a href="#proyects"><button className="btnNav">Proyectos</button></a>
-                    <a href="#contact"><button className="btnNav">Contacto</button></a>
+                    <a href="#home"><button className="btnNav btnNavRespo">Sobre mi</button></a>
+                    <a href="#skills"><button className="btnNav btnNavRespo">Herramientas</button></a>
+                    <a href="#proyects"><button className="btnNav btnNavRespo">Proyectos</button></a>
+                    <a href="#contact"><button className="btnNav btnNavRespo">Contacto</button></a>
                 </nav>
 </div>
     )
--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Card, Button, CardTitle, CardText, Row, Col, CardImg } from 'reactstrap';
 import './components.css';
 import ScrollableAnchor from 'react-scrollable-anchor'
+import burger from '../images/burger.png';
+import pokemon from '../images/pokemon.jpg';
+import woman from '../images/mujeres.jpg';
 
 class Proyects extends Component {
   render() {
@@ -16,7 +19,7 @@ class Proyects extends Component {
             <Col sm="4">
               <Card body>
                 <CardTitle className="cardTitle">Burger Queen</CardTitle>
-                <CardImg top width="100%" src="https://lh5.googleusercontent.com/proxy/ZXvakVk6taUUxk5B0h7vfGeJ6hrLK13KKaqTQ-Zncf6qEmR1-NK5v7FjMdaS_VQV7PfA1cuRRkBui9tpEN2XCAaUYA" alt="Burger Queen" />
+                <CardImg top width="100%" height="140" src={burger} alt="Burger Queen" />
                 <CardText>Proyecto para restaurant, para toma de pedidos y preparación de pedidos.</CardText>
                 <Button color="primary"
                   href="https://burger-queen-9b026.firebaseapp.com/"
@@ -31,7 +34,7 @@ class Proyects extends Component {
             <Col sm="4">
               <Card body>
                 <CardTitle className="cardTitle">Maestro Pokemon Go</CardTitle>
-                <CardImg top width="100%" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d2596d0e-8292-46b8-ad65-d5ec8d67f020/d3ei2p6-2c7a7cd7-c5e5-403f-baf2-ecccac3ba278.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2QyNTk2ZDBlLTgyOTItNDZiOC1hZDY1LWQ1ZWM4ZDY3ZjAyMFwvZDNlaTJwNi0yYzdhN2NkNy1jNWU1LTQwM2YtYmFmMi1lY2NjYWMzYmEyNzguanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.n3hhHRZ0UITG4nvmAcdziiq51jmz2KzYWnJcAgjEKQY" alt="Maestro Pokemon GO" />
+                <CardImg top width="100%" height="140" src={pokemon} alt="Maestro Pokemon GO" />
                 <CardText>Aplicación para jugadores de Pokemon Go, con data de la primera generación.</CardText>
                 <Button color="primary"
                   href="https://palomazelada.github.io/SCL011-data-lovers/src/index.html"
@@ -46,7 +49,7 @@ class Proyects extends Component {
             <Col sm="4">
               <Card body>
                 <CardTitle className="cardTitle">Feminist PWR</CardTitle>
-                <CardImg top width="100%" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d2596d0e-8292-46b8-ad65-d5ec8d67f020/d3ei2p6-2c7a7cd7-c5e5-403f-baf2-ecccac3ba278.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2QyNTk2ZDBlLTgyOTItNDZiOC1hZDY1LWQ1ZWM4ZDY3ZjAyMFwvZDNlaTJwNi0yYzdhN2NkNy1jNWU1LTQwM2YtYmFmMi1lY2NjYWMzYmEyNzguanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.n3hhHRZ0UITG4nvmAcdziiq51jmz2KzYWnJcAgjEKQY" alt="Feminist PWR" />
+                <CardImg top width="100%" height="140" src={woman} alt="Feminist PWR" />
                 <CardText>Red social creada para mujeres para tratar temas de feminismo y de minorias</CardText>
                 <Button color="primary"
                   href="https://palomazelada.github.io/SCL011-data-lovers/src/index.html"
