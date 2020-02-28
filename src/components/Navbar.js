18d0eb7191d4f5b7071db167b723d1028e02afed
PalomaZelada <p.zelada.m@gmail.com>
2020-02-27 21:43:53 -0300

    Completando responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Navfunct from '../Router';
 import './components.css';
 
 
@@ -9,7 +6,6 @@ class Navbar1 extends Component {
   render() {
     return (
       <div>
-      {/* <Navfunct /> */}
       <nav className="navbar-style">
                     <a href="#home"><button className="btnNav">Sobre mi</button></a>
                     <a href="#skills"><button className="btnNav">Herramientas</button></a>
